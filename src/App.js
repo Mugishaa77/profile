f8bfa03ecb5d8456a45bf6b96e1f651cba5d8cb3
Mugishaa77 <sallywanga2016@gmail.com>
2024-08-23 18:55:49 +0300

    Added Light Mode and Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 // pages
@@ -14,27 +15,36 @@ import Evergreen from './projects/Evergreen';
 import Darasani from './projects/Darasani';
 
 function App() {
+  const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
     <div className="App">
-
       <BrowserRouter>
+        <Navbar toggleTheme={toggleTheme} theme={theme} />
 
-      <Navbar />
+        <Routes>
+          {/* Pages */}
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/projects" exact element={<Projects />} />
+          <Route path="/contact" exact element={<Contacts />} />
 
-      <Routes>
-        {/* Pages */}
-        <Route path="/" exact element={<Home />} />
-        <Route path="/about" exact element = {<About />} />
-        <Route path="/projects" exact element = {<Projects/>} />
-        <Route path="/contact" exact element = {<Contacts/>}/>
-
-        {/* Projects */}
-        <Route path="/atlas" exact element= {<Atlas/>}/>
-        <Route path="/evergreen" exact element={<Evergreen/>}/>
-        <Route path="/darasani" exact element={<Darasani />} />
-      </Routes>
+          {/* Projects */}
+          <Route path="/atlas" exact element={<Atlas />} />
+          <Route path="/evergreen" exact element={<Evergreen />} />
+          <Route path="/darasani" exact element={<Darasani />} />
+        </Routes>
       </BrowserRouter>
-    
     </div>
   );
 }
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,7 +1,7 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Mugisha from '../media/Mugisha.png';
 
-export default function Navbar() {
+export default function Navbar({ toggleTheme, theme }) {
   return (
     <nav className="lusitana-bold">
       <img src={Mugisha} alt="Mugisha" />
@@ -39,11 +39,34 @@ export default function Navbar() {
           </NavLink>
         </li>
         <li>
-        <a href="https://github.com/Mugishaa77" target="_blank" rel="noopener noreferrer">Github<sup>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
-  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
-</svg></sup></a>
+          <a href="https://github.com/Mugishaa77" target="_blank" rel="noopener noreferrer">
+            Github
+            <sup>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-box-arrow-up-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
+                />
+                <path
+                  fillRule="evenodd"
+                  d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
+                />
+              </svg>
+            </sup>
+          </a>
+        </li>
+        <li>
+          {/* Theme toggle button */}
+          <button onClick={toggleTheme} style={{ marginLeft: '20px' }}>
+            Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+          </button>
         </li>
       </ul>
     </nav>
